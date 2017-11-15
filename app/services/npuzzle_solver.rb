8e17d5aac76e6698d3a6e75aae6afd9a45f9d49a
Gregory Havenga <gregory.havenga@gmail.com>
2017-11-15 13:38:56 +0200

    Various improvements

--- app/services/npuzzle_solver.rb
+++ app/services/npuzzle_solver.rb
@@ -9,8 +9,9 @@ class NpuzzleSolver
               }
 
     while solution[:solution].empty?
-      puzzlesize = rand(3..5)
-      puts puzzlesize
+      # puzzlesize = rand(3..5)
+      puzzlesize = 3
+      puts "Puzzle Size = #{puzzlesize}"
       threepuzzle = [0, 1, 2, 3, 4, 5, 6, 7, 8]
       fourpuzzle = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
       fivepuzzle = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
